71151295ddab1217b2d17fd8445599004abd2b6e
SafaENSI <safa.bensalah@ensi-uma.tn>
2022-06-21 17:48:17 +0200

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,9 +1,2 @@
-cd C:\Users\bensa\PycharmProjects\pythonProject2\
+cd C:/Users/bensa/PycharmProjects/pythonProject2/
 python Running_Canoe.py
-
-if errorlevel 1 (
- 	echo %errorlevel% failed test modules
-) else (
-	echo all test modules successfully executed
-)
-exit /b 0
