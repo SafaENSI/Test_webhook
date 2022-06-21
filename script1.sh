cb9febf221ad8cc69c6c076cdf1dbc42e0832a07
SafaENSI <safa.bensalah@ensi-uma.tn>
2022-06-21 17:44:02 +0200

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,9 +1,6 @@
-@echo off
-
 cd C:\Users\bensa\PycharmProjects\pythonProject2\
 python Running_Canoe.py
 
-rem Output number of errors in tests, but do not signal to Jenkins
 if errorlevel 1 (
  	echo %errorlevel% failed test modules
 ) else (
