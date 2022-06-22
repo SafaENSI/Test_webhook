bacc7f8a666a43043344a1aaab58c259b412547e
safa Ben Salah <safa.bensalah@ensi-uma.tn>
2022-06-22 09:20:08 +0200

    Check jenkins_canoe connection3

--- script1.sh
+++ script1.sh
@@ -1,2 +1,10 @@
-python3 --version
-python3 main.py
+@echo off
+python main.py
+
+rem Output number of errors in tests, but do not signal to Jenkins
+if errorlevel 1 (
+ 	echo %errorlevel% failed test modules
+) else (
+	echo all test modules successfully executed
+)
+exit /b 0
