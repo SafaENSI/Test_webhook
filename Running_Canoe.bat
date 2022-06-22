bbc795dbe96e50322993c0343565358b919b3e29
SafaENSI <safa.bensalah@ensi-uma.tn>
2022-06-22 09:39:56 +0200

    Update Running_Canoe.bat

--- Running_Canoe.bat
+++ Running_Canoe.bat
@@ -1,13 +1,3 @@
-@echo off
-echo Running script ..
 cd C:\Users\bensa\PycharmProjects\pythonProject2\
-echo Running script ...
 python3 Running_Canoe.py
 
-rem Output number of errors in tests, but do not signal to Jenkins
-if errorlevel 1 (
- 	echo %errorlevel% failed test modules
-) else (
-	echo all test modules successfully executed
-)
-exit /b 0
